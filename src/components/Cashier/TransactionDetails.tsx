db7f8a51fbc233456ade390fd56758bd0c3fd5d4
Joshua Orbiso <joshuaorb372@gmail.com>
2023-10-13 22:17:40 +0800

    Some Changes

--- src/components/Cashier/TransactionDetails.tsx
+++ src/components/Cashier/TransactionDetails.tsx
@@ -9,16 +9,38 @@ interface Product {
     price: number;
 }
 
+interface TransactionDetails {
+    transactionid: number;
+    total_quantity: number;
+    total_price: number;
+    tendered_bill: number;
+    balance: number;
+    customer_name: string;
+    customer_num: string;
+    customer_email: string;
+    date_time: string;
+}
+
 const TransactionDetails = () => {
     const { id } = useParams();
+    const [transactionDetails, setTransactionDetails] = useState<TransactionDetails | null>();
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
+        // Fetch transaction details using the provided URL
+        axios.get(`http://localhost:8080/transaction/getByTransaction?transactionid=${id}`)
+            .then((response) => {
+                const responseData: TransactionDetails = response.data;
+                setTransactionDetails(responseData);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+
+        // Fetch products related to this transaction
         axios.get(`http://localhost:8080/transaction/${id}/products`)
             .then((response) => {
-                const responseData: Product[] = response.data; // Assumes the response is an array of Product
-
-                // Set product details
+                const responseData: Product[] = response.data;
                 setProducts(responseData);
             })
             .catch((error) => {
@@ -28,6 +50,49 @@ const TransactionDetails = () => {
 
     return (
         <div>
+            <h2>Transaction Details</h2>
+            {transactionDetails && (
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>Transaction ID</th>
+                            <td>{transactionDetails.transactionid}</td>
+                        </tr>
+                        <tr>
+                            <th>Total Quantity</th>
+                            <td>{transactionDetails.total_quantity}</td>
+                        </tr>
+                        <tr>
+                            <th>Total Price</th>
+                            <td>{transactionDetails.total_price}</td>
+                        </tr>
+                        <tr>
+                            <th>Tendered Bill</th>
+                            <td>{transactionDetails.tendered_bill}</td>
+                        </tr>
+                        <tr>
+                            <th>Balance</th>
+                            <td>{transactionDetails.balance}</td>
+                        </tr>
+                        <tr>
+                            <th>Customer Name</th>
+                            <td>{transactionDetails.customer_name}</td>
+                        </tr>
+                        <tr>
+                            <th>Customer Number</th>
+                            <td>{transactionDetails.customer_num}</td>
+                        </tr>
+                        <tr>
+                            <th>Customer Email</th>
+                            <td>{transactionDetails.customer_email}</td>
+                        </tr>
+                        <tr>
+                            <th>Date & Time</th>
+                            <td>{transactionDetails.date_time}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            )}
             <h2>Products Purchased</h2>
             <table>
                 <thead>
--- src/components/Cashier/TransactionHistory.tsx
+++ src/components/Cashier/TransactionHistory.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import './TransactionHistory.css';
 
 interface Transaction {
     transactionid: number;
-    total_quantity: number;
-    total_price: number;
-    tendered_bill: number;
-    balance: number;
     date_time: string;
 }
 
@@ -26,51 +23,41 @@ const TransactionHistory = () => {
     }, []);
 
     const handleDeleteTransaction = (id: number) => {
-        const confirmDelete = window.confirm("Are you sure you want to delete this transaction?");
-
-        if (confirmDelete) {
-            axios.delete(`http://localhost:8080/transaction/deleteTransaction/${id}`)
-                .then(() => {
-                    setTransactionDeleted(true); // Set a flag to indicate that the transaction has been deleted
-                    setTransactions((prevTransactions) =>
-                        prevTransactions.filter((transaction) => transaction.transactionid !== id)
-                    );
-                })
-                .catch((error) => {
-                    console.error(error);
-                });
-        }
-    };
+      const confirmDelete = window.confirm("Are you sure you want to delete this transaction?");
+  
+      if (confirmDelete) {
+          axios.delete(`http://localhost:8080/transaction/deleteTransaction/${id}`)
+              .then(() => {
+                  // Display a window.confirm message after successful deletion
+                  const success = window.confirm("Transaction has been deleted.");
+                  if (success) {
+                      setTransactions((prevTransactions) =>
+                          prevTransactions.filter((transaction) => transaction.transactionid !== id)
+                      );
+                  }
+              })
+              .catch((error) => {
+                  console.error(error);
+              });
+      }
+  };
 
     return (
         <div>
-            <h1>Transaction History</h1>
-            {isTransactionDeleted && (
-                <div>
-                    <p>Transaction has been deleted.</p>
-                </div>
-            )}
-            <table>
+            <h1 style={{ marginLeft: '90px' , marginBottom: '10px', marginTop: '30px'}}>All Transactions</h1>
+            <table className="transaction-table">
                 <thead>
                     <tr>
                         <th>Transaction ID</th>
-                        <th>Total Quantity</th>
-                        <th>Total Price</th>
-                        <th>Tendered Bill</th>
-                        <th>Balance</th>
                         <th>Date & Time</th>
-                        <th>View</th>
-                        <th>Delete</th>
+                        <th></th>
+                        <th></th>
                     </tr>
                 </thead>
                 <tbody>
                     {transactions.map((transaction) => (
                         <tr key={transaction.transactionid}>
                             <td>{transaction.transactionid}</td>
-                            <td>{transaction.total_quantity}</td>
-                            <td>{transaction.total_price}</td>
-                            <td>{transaction.tendered_bill}</td>
-                            <td>{transaction.balance}</td>
                             <td>{transaction.date_time}</td>
                             <td>
                                 <Link to={`/transactions/${transaction.transactionid}`}>
