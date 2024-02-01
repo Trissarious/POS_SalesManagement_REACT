d2cbe14b33ed1c324d954a792ec9aa7201d2b8a0
Trisha Joy Oballo <116042598+Trissarious@users.noreply.github.com>
2024-02-01 21:06:05 +0800

    ff

--- src/components/Cashier/Transaction_Details.tsx
+++ src/components/Cashier/Transaction_Details.tsx
@@ -72,7 +72,7 @@ const Transaction_Details = () => {
       const token = localStorage.getItem('cashierToken');
   
       if (!token) {
-        navigate('/logincashier');
+        navigate('/logincash');
       } else {
       }
     }, [isCashierLoggedIn, navigate]);
@@ -93,7 +93,7 @@ const Transaction_Details = () => {
         const token = localStorage.getItem('cashierToken');
     
         if (!token) {
-          navigate('/logincashier');
+          navigate('/logincash');
         } else {
         }
       }, [isCashierLoggedIn, navigate]);
